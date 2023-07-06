fa51663fa4229c8733e994900206fe4e428da8e8
Sudharsan Reddy Katha <138109855+KathaSudharshan@users.noreply.github.com>
2023-07-06 12:13:28 +0100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@GetMapping("/hello")
+	@GetMapping("/demo")
 	public String hello(@RequestParam(value = "name", defaultValue = "Sudharshan") String name) {
 	      return String.format("Hello %s! from Spring boot application", name);
 	    }
